0b1b6bcb794924cf91bf0ba28fe560ba7450242f
tkubica12 <tkubica12@gmail.com>
2019-01-14 18:37:04 +0100

    initial

---
+++ greetings/index.js
@@ -0,0 +1,15 @@
+module.exports = async function (context, req) {
+    context.log('Received request');
+
+    if (req.body && req.body.name) {
+        context.res = {
+            body: "Hello " + req.body.name
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+};
